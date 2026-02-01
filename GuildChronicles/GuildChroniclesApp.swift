8b316657e046ab6cc2bdeb1123d536ad794eb309
tom <tommy.yohanes@icloud.com>
2026-02-01 13:50:44 +0700

    Initial Commit

---
+++ GuildChronicles/GuildChroniclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuildChroniclesApp.swift
+//  GuildChronicles
+//
+//  Created by tomtomtom on 2/1/26.
+//
+
+import SwiftUI
+
+@main
+struct GuildChroniclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GuildChroniclesTests/GuildChroniclesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GuildChroniclesTests.swift
+//  GuildChroniclesTests
+//
+//  Created by tomtomtom on 2/1/26.
+//
+
+import Testing
+@testable import GuildChronicles
+
+struct GuildChroniclesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
